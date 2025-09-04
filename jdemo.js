7987571a390a976a53eeccc4b01a0db211233f9e
anjumatif <shaikhanjum4221@gmail.com>
2025-09-04 10:41:57 +0100

    javademo

---
+++ jdemo.js
@@ -0,0 +1,18 @@
+console.log("welcome to my javascriptdemo")
+
+let myName="Anjum";
+console.log(myName)
+
+let age = 45
+console.log(age)
+
+let numberOne = 19
+let numbertwo = 25
+let sum = numberOne + numbertwo
+console.log(sum)
+
+let myname= "Atif"
+let sname="Shaikh"
+
+console.log(`My fullname is ${myName} ${myname} ${sname}`)
+
