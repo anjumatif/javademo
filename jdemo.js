2031b31062a7155ae0b8b052e5eafd24b7d78cdf
anjumatif <shaikhanjum4221@gmail.com>
2025-09-04 11:26:31 +0100

    updateddemo

--- jdemo.js
+++ jdemo.js
@@ -14,5 +14,42 @@ console.log(sum)
 let myname= "Atif"
 let sname="Shaikh"
 
+//string literals
 console.log(`My fullname is ${myName} ${myname} ${sname}`)
+
+//string together using + operator
+let fullName= myName + " " + myname + " " + sname
+console.log(fullName)
+
+//no of character in string
+console.log(`${myName} is ${myName.length} characters long`)
+console.log(`${myname} is ${myname.length} characters long`)
+
+//booleans
+let isTrue = true
+console.log(isTrue)
+let isFalse = false
+console.log(isFalse)
+
+//typeof operator
+console.log(typeof(myName))
+
+console.log("Type of numberone is " + typeof numberOne)
+
+console.log(`Type of isTrue is ${typeof isTrue}`)
+
+
+//undefined null
+let name
+console.log(name)
+
+let myage=null
+console.log(myage)
+
+//comparisons
+let a = 10
+let b = 25
+let isAgreaterthanB = a > b
+console.log(isAgreaterthanB)
+
 
